682b807906d01cf5d0dc1d56257a9130b6896f23
antoid <antoidco@gmail.com>
2020-11-25 17:14:07 +0300

    TurbulentWindModel fixed

--- Assets/Scripts/Simulator/TurbulentWindModel.cs
+++ Assets/Scripts/Simulator/TurbulentWindModel.cs
@@ -4,12 +4,13 @@ namespace AircraftSimulator
 {
     public class TurbulentWindModel : Model
     { 
-        public TurbulentWindModel(Vector3 PositionValue) : base(ModelType.Wind)
-        {
-            var x = PositionValue.x;
-            var y = PositionValue.y;
-            var z = PositionValue.z;
-            var WindValue = Vector3.zero;
+        public TurbulentWindModel() : base(ModelType.Wind) { }
+
+        public Vector3 Value(Vector3 position, double time) {
+            var x = position.x;
+            var y = position.y;
+            var z = position.z;
+            var windValue = Vector3.zero;
 
             var su = 2;
             var sv = 1;
@@ -19,13 +20,11 @@ namespace AircraftSimulator
             var Lv = 16;
             var Lw = 4;
 
-            WindValue.x = su * su * Lu / (Mathf.PI * (1 + Mathf.Pow(Lu * x, 2)));
-            WindValue.y = sv * sv * Lv * (1 + 12 * Mathf.Pow(Lv * y, 2)) / Mathf.Pow(Mathf.PI * (1 + 4 * Mathf.Pow(Lv * y, 2)), 2);
-            WindValue.z = sw * sw * Lw * (1 + 12 * Mathf.Pow(Lw * z, 2)) / Mathf.Pow(Mathf.PI * (1 + 4 * Mathf.Pow(Lw * z, 2)), 2);
+            windValue.x = su * su * Lu / (Mathf.PI * (1 + Mathf.Pow(Lu * x, 2)));
+            windValue.y = sv * sv * Lv * (1 + 12 * Mathf.Pow(Lv * y, 2)) / Mathf.Pow(Mathf.PI * (1 + 4 * Mathf.Pow(Lv * y, 2)), 2);
+            windValue.z = sw * sw * Lw * (1 + 12 * Mathf.Pow(Lw * z, 2)) / Mathf.Pow(Mathf.PI * (1 + 4 * Mathf.Pow(Lw * z, 2)), 2);
 
-            Value = WindValue;
+            return windValue;
         }
-
-        public Vector3 Value { get; set; }
     }
 }
