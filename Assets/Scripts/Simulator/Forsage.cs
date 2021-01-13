09fe1c07e14b5d82517a61067fe83b5baa239b58
antoid <antoidco@gmail.com>
2021-01-13 23:09:58 +0300

    Disable forsage after 1 second

--- Assets/Scripts/Simulator/Forsage.cs
+++ Assets/Scripts/Simulator/Forsage.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace AircraftSimulator {
     public class Forsage : MonoBehaviour {
-        public bool IsActive = false;    
+        public bool IsActive = false;
+        
+        /* here we need to add some code to disable Forsage after 1 second, for example
+         * in Unity it is some kind of tricky, you need to use Coroutines in order to "wait" in background
+         * https://docs.unity3d.com/Manual/Coroutines.html
+         * please, read this article and try to complete the code
+        */
+        
+        // this is coroutine
+        IEnumerator DisableAfterOneSecond() 
+        {
+            yield return new WaitForSeconds(1f); // here we wait for 1 second
+            IsActive = false; // here we disable forsage
+            Debug.Log("Forsage disabled");
+        }
         public void ButtonForsage() {
             IsActive = true;
             Debug.Log("Forsage!");
+        
+            /*past your code here*/.("DisableAfterOneSecond");
         }
     }
 }
