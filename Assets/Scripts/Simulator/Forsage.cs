7c6ab0c00504d9f287ab9a86acea6e4c34c0de98
prohogii <olpd2000@yandex.ru>
2021-03-30 20:20:27 +0300

    Update Forsage.cs

--- Assets/Scripts/Simulator/Forsage.cs
+++ Assets/Scripts/Simulator/Forsage.cs
@@ -1,20 +1,55 @@
-﻿using System;
+using System;
 using System.Collections;
 using UnityEngine;
 
 namespace AircraftSimulator {
     public class Forsage : MonoBehaviour {
+        public int j = 1;
+        public int k = 1;
+        public bool isDisactive = false;
         public bool IsActive = false;
-        IEnumerator DisableAfterOneSecond(){
-            yield return new WaitForSeconds(5f); // here we wait for 5 second
-            IsActive = false; // here we disable forsage
-            Debug.Log("Forsage disabled");
+        IEnumerator DisableAfterTwoSeconds()
+        {
+            while (k <= 25)
+            {
+
+
+                yield return new WaitForSeconds(0.1f); // here we wait for 0.1 second
+
+
+                k++;
+            }
+
+
+            isDisactive = false;
+             Debug.Log("Forsage disabled");
+
         }
-        public void ButtonForsage() {
-            IsActive = true;
-            Debug.Log("Forsage!");
+        IEnumerator DisableAfterOneSecond(){
+            while (j <= 25)
+            {
+               
+                
+                yield return new WaitForSeconds(0.1f); // here we wait for 0.1 second
+
+                 
+            j++;
+            }
+            IsActive = false; // here we disable forsage
+            isDisactive = true;
+            StartCoroutine("DisableAfterTwoSeconds");
+           
+
+        }
             
+        public void ButtonForsage() {
+            Debug.Log("Forsage!");
+            IsActive = true;
+
+
             StartCoroutine("DisableAfterOneSecond");
+
+
         }
     }
 }
